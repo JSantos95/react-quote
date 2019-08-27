a8213fb18200628178d8612fb5ac1a6e046b5382
Jose <santosjosel1995@gmail.com>
2019-08-26 21:12:30 -0400

    Finished Project. Calling API for a new quote and generating a new color

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            text: '',
+            author: '',
+            color: ''
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    componentDidMount(){
+      let x = Math.floor(Math.random() * 255);
+      let y = Math.floor(Math.random() * 255);
+      let z = Math.floor(Math.random() * 255);
+      fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+        .then(response => response.json())
+        .then(response =>{
+          const quote = response;
+          console.log(quote);
+          this.setState({
+            text: quote.en,
+            author: quote.author,
+            color: 'rgb(' + x.toString() + ',' + y.toString() + ',' + z.toString() + ')'
+          })
+        })
+    }
+
+    handleClick(){
+      let x = Math.floor(Math.random() * 255);
+      let y = Math.floor(Math.random() * 255);
+      let z = Math.floor(Math.random() * 255);
+      fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+        .then(response => response.json())
+        .then(response =>{
+          const quote = response;
+          console.log(quote);
+          this.setState({
+            text: quote.en,
+            author: quote.author,
+            color: 'rgb(' + x.toString() + ',' + y.toString() + ',' + z.toString() + ')'
+          })
+        })
+    }
+
+    render(){
+        return(
+            <div
+              className="bodyy"
+              style={{backgroundColor: this.state.color}}>
+                <div className='content' id="quote-box">
+                    <h1 id="text">{this.state.text}</h1>
+                    <h5 id="author">- {this.state.author}</h5>
+                    <button
+                        id="tweet-quote"
+                        className='lbutton'>
+                        <a
+                          href={"twitter.com/intent/tweet?text=" + this.state.text}
+                          target="_blank"
+                          rel = "noopener noreferrer"
+                          style={{color: this.state.color, fontSize: '32px' }}>
+                          <FontAwesomeIcon icon={faTwitter}/>
+                        </a>
+                    </button>
+                    <button
+                        id="new-quote"
+                        className='rbutton'
+                        onClick={this.handleClick}
+                        style={{backgroundColor: this.state.color}}>
+                        New Quote
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
